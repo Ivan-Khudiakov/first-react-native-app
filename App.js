10a63666f876668debd131425e95a2f47371ad4d
Ivan-Khudiakov <khudiakov1990@gmail.com>
2021-09-05 21:13:35 +0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import styled from 'styled-components/native'
+import {Group} from "./components/Group/Group";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Container>
+            <Group groupTitle='14 september' items={[
+                {
+                    time: '12.30',
+                    diagnosis: 'пульпит',
+                    user: {
+                        fullName: 'Василий Рогов',
+                        avatar: 'https://www.ejin.ru/wp-content/uploads/2018/10/crew4_1024.png'}
+                },
+                {
+                    time: '13.30',
+                    diagnosis: 'флюс',
+                    user: {
+                        fullName: 'Андрей Ларин',
+                        avatar: 'https://bipbap.ru/wp-content/uploads/2021/07/1551512888_2.jpg'}
+                },
+                {
+                    time: '14.30',
+                    diagnosis: 'удаление зуба',
+                    user: {
+                        fullName: 'Анатолий Дукалис',
+                        avatar: 'https://i06.fotocdn.net/s129/bb01329b8ba1efc7/user_xl/2924033751.jpg'}
+                },
+            ]}/>
+        </Container>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  margin-top: 60px;
+`;
